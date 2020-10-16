7a96a7cfc49c5fea075c43124aede147165e247e
Pedro Henrique <peucopasa@gmail.com>
2020-10-16 15:49:37 -0300

    correções layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -149,21 +149,21 @@
 
     </div>
 </body>
-    @yield('scrip')
+    @stack('js')
         <!-- Bootstrap core JavaScript-->
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
 
         <!-- Core plugin JavaScript-->
-        <script src="{{ asset('js/jquery.easing.min.js') }}"></script>
+        <script src="{{ asset('/js/jquery.easing.min.js') }}"></script>
 
         <!-- Custom scripts for all pages-->
-        <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+        <script src="{{ asset('/js/sb-admin-2.min.js') }}"></script>
 
         <!-- Page level plugins -->
-        <script src="{{ asset('js/Chart.min.js') }}"></script>
+        <script src="{{ asset('/js/Chart.min.js') }}"></script>
 
         <!-- Page level custom scripts -->
-        <script src="{{ asset('js/chart-area-demo.js') }}"></script>
-        <script src="{{ asset('js/chart-pie-demo.js') }}"></script>
-
+        <script src="{{ asset('/js/chart-area-demo.js') }}"></script>
+        <script src="{{ asset('/js/chart-pie-demo.js') }}"></script>
+</html>    
