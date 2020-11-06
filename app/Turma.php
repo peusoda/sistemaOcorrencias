033328b47adfb49a1d145520cc6f559cf12cb106
Sypriano <syprianom@gmail.com>
2020-11-05 21:05:14 -0300

    Add migrations de Turma, tela create e index de turma, controller de turma

---
+++ app/Turma.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Turma extends Model
+{
+    protected $fillable = [
+    	'id',
+    	'codigo',
+    	'turma',
+    ];
+}
