a589601f1668bab964b2f8bcbb77ecb5206fe8e1
Pedro Henrique <peucopasa@gmail.com>
2020-12-03 20:52:25 -0300

    Criação do model, controller e migration da tabela Aluno

--- app/Http/Controllers/AlunosController.php
+++ app/Http/Controllers/AlunosController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Aluno;
 class AlunosController extends Controller
 {
-    //
+    protected function show() {
+        $alunos = new Aluno();
+        $alunos = Aluno::all();
+
+        return view('dashboard.aluno.show')
+            ->with('alunos', $alunos);
+    }
+    protected function create() {
+        return view('dashboard.aluno.create');
+    }
 }
