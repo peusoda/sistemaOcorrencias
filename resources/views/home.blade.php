60f4b95af8fd4fbbab2c0724a011a683565af440
vithugo09 <vitinhojeki133@gmail.com>
2020-12-07 20:27:28 -0300

    Links e CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
         <div class="carousel-caption text-left">
           <h1>Example headline.</h1>
           <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Nova Ocorrência</a></p>
+          <p><a class="btn btn-lg btn-primary" href="{{ route('ocorrencia.create') }}" role="button">Nova Ocorrência</a></p>
         </div>
       </div>
     </div>
@@ -45,7 +45,7 @@
         <div class="carousel-caption text-right">
           <h1>One more for good measure.</h1>
           <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Conheça o Campus</a></p>
+          <p><a class="btn btn-lg btn-primary" href="https://www.ifnmg.edu.br/almenara" target="_blank" role="button">Conheça o Campus</a></p>
         </div>
       </div>
     </div>
