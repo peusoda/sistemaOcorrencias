8a4e320cca1010a75109d231ec485d70be5ccc1f
Pedro Henrique <peucopasa@gmail.com>
2020-12-17 00:43:55 -0300

    Finalizando associação dos CRUDS Responsaveis e Users

--- database/migrations/2020_12_17_031006_add_dados_to_users_table.php
+++ database/migrations/2020_12_17_031006_add_dados_to_users_table.php
@@ -14,7 +14,12 @@ class AddDadosToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('responsavel_id')->nullable();
+            $table->foreign('responsavel_id')
+                ->references('id')
+                ->on('responsavels')
+                ->onDelete('cascade');
+            $table->enum('tipo', ['admin', 'servidor', 'responsavel', 'aluno'])->nullable($value = true);
         });
     }
 
